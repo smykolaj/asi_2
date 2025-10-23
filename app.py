aa35bcff2789ccf5580f3d55952bb6724934cc8d
Mykola Sukonnik <134325953+smykolaj@users.noreply.github.com>
2025-10-23 14:40:33 +0000

    Init Commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+from predict import make_prediction
+
+st.title("Iris prediction app")
+
+f1 = st.slider(label='SepalLengthCm',min_value=4.3,max_value=7.9)
+f2 = st.slider(label='SepalWidthCm',min_value=2.0,max_value=4.4)
+f3 = st.slider(label='PetalLengthCm',min_value=1.0,max_value=6.9)
+f4 = st.slider(label='PetalWidthCm',min_value=1.0,max_value=2.5)
+
+prediction = make_prediction(f1, f2, f3, f4)
+st.markdown(f'### Prediction: {prediction}')
---
+++ predict.py
@@ -0,0 +1,31 @@
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+import joblib
+import os
+
+filename = 'model.joblib'
+
+def train_model():
+    iris = load_iris()
+
+    X = iris.data
+    y = iris.target
+
+    X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=42)
+
+    model = LogisticRegression(max_iter=200)
+    model.fit(X_train, y_train)
+    
+    joblib.dump(model, filename)
+    
+def get_model():
+    if not os.path.exists(filename):
+        train_model()
+    return joblib.load('model.joblib')
+
+def make_prediction(*args):
+    prediction = get_model().predict(args)
+    return prediction
